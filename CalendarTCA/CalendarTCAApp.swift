fe6dc7c855765db36a085fb5ecf6ea435f3353ff
Maksim Omel'chenko <omelchenko@mts.ru>
2024-11-15 12:36:14 +0300

    Initial Commit

---
+++ CalendarTCA/CalendarTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarTCAApp.swift
+//  CalendarTCA
+//
+//  Created by Омельченко Максим on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalendarTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
